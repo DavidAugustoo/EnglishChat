7d53bec618d7be3d94ce3fb4bb7c2fe95542a21f
David Augusto <davidribeiro086@gmail.com>
2022-06-17 18:15:40 -0300

    :art: style: Added styling files

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,14 @@
 import express from 'express';
 import path from 'path';
-import mustache from 'mustache';
 
+// Server Config
 const app = express();
 
-app.set('view engine', 'mustache');
-app.set('views', path.join(__dirname, 'views'));
-app.engine('mustache', mustache());
-
 app.use(express.static(path.join(__dirname, '../public')));
-
 app.use(express.urlencoded({extended: true}));
 
-// routes
-app.use();
+// Routes Config
 
+// Starting Server
 var port_number = app.listen(process.env.PORT || 8080);
-
 app.listen(port_number);
